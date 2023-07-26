24c5091c2b23a441605a777a8d39ca50dc2d7442
Sylvain <slorange7@gmail.com>
2023-07-26 01:23:30 -0400

    Changed direction and movement angle based on camera
    Also fixed initial delay on keyboard presses

--- main.js
+++ main.js
@@ -47,6 +47,19 @@ loader.load(
   }
 );
 
+let car;
+loader.load(
+  'AM_DB2.glb',
+  function (gltf) {
+    car = gltf.scene;
+    scene.add(car);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
 //camera controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.mouseButtons = {
@@ -56,33 +69,81 @@ controls.mouseButtons = {
 controls.update();
 
 
-//moving duck
-var speed = 1;
+//get key input
+var leftPressed, rightPressed, downPressed, upPressed;
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
   var keyCode = event.which;
   if (keyCode == 87) {
-    duck.position.z += speed;
-    camera.position.z += speed;
+    upPressed = true;
   }
   if (keyCode == 83) {
-    duck.position.z -= speed;
-    camera.position.z -= speed;
+    downPressed = true;
   }
   if (keyCode == 65) {
-    duck.position.x -= speed;
-    camera.position.x -= speed;
+    leftPressed = true;
   }
   if (keyCode == 68) {
-    duck.position.x += speed;
-    camera.position.x += speed;
+    rightPressed = true;
+  }
+};
+document.addEventListener("keyup", onDocumentKeyUp, false);
+function onDocumentKeyUp(event) {
+  var keyCode = event.which;
+  if (keyCode == 87) {
+    upPressed = false;
+  }
+  if (keyCode == 83) {
+    downPressed = false;
+  }
+  if (keyCode == 65) {
+    leftPressed = false;
+  }
+  if (keyCode == 68) {
+    rightPressed = false;
   }
 };
 
+
 function animate() {
   requestAnimationFrame(animate);
-  controls.target.set(duck.position.x, duck.position.y, duck.position.z);
+  //point camera towards the duck
+  if(duck) controls.target.set(duck.position.x, duck.position.y, duck.position.z);
+
   controls.update();
   renderer.render(scene, camera);
+
+  var azimu = controls.getAzimuthalAngle(); // camera angle around y axis, in radians
+  var deltax = 0, deltaz = 0;
+  var speed = 0.1;
+
+  //set duck rotation and position based on camera angle and keys pressed
+  if (upPressed) {
+    duck.rotation.y = azimu + Math.PI / 2;
+    deltax += -Math.sin(azimu) * speed;
+    deltaz += -Math.cos(azimu) * speed;
+  }
+  if (downPressed) {
+    duck.rotation.y = azimu + 3 * Math.PI / 2;
+    deltax += Math.sin(azimu) * speed;
+    deltaz += Math.cos(azimu) * speed;
+  }
+  if (leftPressed) {
+    duck.rotation.y = azimu +  Math.PI ;
+    deltax += -Math.cos(azimu) * speed;
+    deltaz += Math.sin(azimu) * speed;
+  }
+  if (rightPressed) {
+    duck.rotation.y = azimu;
+    deltax += Math.cos(azimu) * speed;
+    deltaz += -Math.sin(azimu) * speed;
+  }
+  if (duck) {
+    duck.position.x += deltax;
+    duck.position.z += deltaz;
+  }
+  //move camera with duck
+  camera.position.x += deltax;
+  camera.position.z += deltaz;
 }
 animate();
